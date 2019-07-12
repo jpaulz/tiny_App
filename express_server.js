0c653216a5a723ea3ab917835db3972214b09da0
Julia Paulasaari <julia@paulasaari.com>
2019-07-12 03:54:57 +0000

    substituded all username cookie to user_id

--- express_server.js
+++ express_server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use((req, res, next) => {
   res.locals = {
-    username: req.cookies["username"]
+    "user_id": req.cookies["user_id"]
   };
   next();
 });
